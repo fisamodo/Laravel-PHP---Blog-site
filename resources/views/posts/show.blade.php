a86edca584775c136f364056b6f2af9a122e4914
fisamodo <fisamodo@vsite.hr>
2020-04-08 08:42:25 -0700

    update and delete, full crud working

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,12 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
-<a href="/posts" class="btn btn-default">Go Back</a>
+<a href="/posts" class="btn btn-outline-warning">Go Back</a>
     <h1>{{$post->title}}</h1>
     <div>
         {!!$post->body!!}
     </div>
     <hr>
     <small>Written on {{$post->created_at}}</small>
+    <hr>
+    <a href="/posts/{{$post->id}}/edit" class="btn btn-light">Edit</a>
 
+    {!!Form::open(['action'=>['PostsController@destroy',$post->id],'method'=>'POST','class' => 'float-right'])!!}
+        {{Form::hidden('_method', 'DELETE')}}
+        {{Form::submit('Delete',['class'=>'btn btn-outline-danger'])}}
+    {!!Form::close()!!}
 @endsection
