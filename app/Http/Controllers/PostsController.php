0a803fb20319a5ab1785a9354e86f0cf2d128925
fisamodo <fisamodo@vsite.hr>
2020-04-09 09:07:42 -0700

    Done with the image upload thing.

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Post;
 use DB;
+use Illuminate\Support\Facades\Storage;
 
 class PostsController extends Controller
 {
@@ -63,17 +64,17 @@ class PostsController extends Controller
         //Handle File Upload
         if($request->hasFile('cover_image')){
             //GET filename with the extension
-            $filenameWithExt = $request->file('cover_image')->getClientOriginalImage();
+            $filenameWithExt = $request->file('cover_image')->getClientOriginalName();
             //Get just filename
             $filename = pathinfo($filenameWithExt, PATHINFO_FILENAME);
             //get just extension
-            $extension = $request->file('cover_image')->getOriginalClientExtension();
+            $extension = $request->file('cover_image')->getClientOriginalExtension();
             //filename to store
             $fileNameToStore = $filename.'_'.time().'.'.$extension;
             //upload image
             $path = $request->file('cover_image')->storeAs('public/cover_images',$fileNameToStore);
         }else{
-            $fileNameToStore ='noimage.jpeg';
+            $fileNameToStore ='noimage.jpg';
         }
         //Create post
         $post = new Post;
@@ -129,10 +130,28 @@ class PostsController extends Controller
             'title' =>'required',
             'body'=> 'required'
         ]);
+
+        //Handle File Upload
+        if($request->hasFile('cover_image')){
+            //GET filename with the extension
+            $filenameWithExt = $request->file('cover_image')->getClientOriginalName();
+            //Get just filename
+            $filename = pathinfo($filenameWithExt, PATHINFO_FILENAME);
+            //get just extension
+            $extension = $request->file('cover_image')->getClientOriginalExtension();
+            //filename to store
+            $fileNameToStore = $filename.'_'.time().'.'.$extension;
+            //upload image
+            $path = $request->file('cover_image')->storeAs('public/cover_images',$fileNameToStore);
+        }
         //Create post
         $post = Post::find($id);
         $post->title = $request->input('title');
         $post->body = $request->input('body');
+        if($request->hasFile('cover_image')){
+        $post->cover_image = $fileNameToStore;
+        }
+
         $post->save();
 
         return redirect('/posts')->with('success', 'Post Updated');
@@ -151,6 +170,12 @@ class PostsController extends Controller
         if(auth()->user()->id !==$post->user_id){
             return redirect('/posts')->with('error','Unauthorized page');
         }
+
+        if($post->cover_image != 'noimage.jpg'){
+            //Delete Image
+            Storage::delete('public/cover_images/'.$post->cover_image);
+        }
+
         $post->delete();
         return redirect('/posts')->with('success','Post Removed');
     }
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,9 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
+@if(Auth::guest())
+
 <div class="jumbotron text-center">
 <h1>{{$title}}</h1>
 <p>This is the laravel app</p>
 <p><a class="btn btn-primary btn-lg" href="/login" role="button">Login</a>
 <a class="btn btn-success btn-lg" href="/register" role="button">Register</a></p>
+@endif
 @endsection
