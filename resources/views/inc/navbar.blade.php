fcb6485f31789a890e5ca424f7bdab5735462ebf
fisamodo <fisamodo@vsite.hr>
2020-04-09 03:15:43 -0700

    authentication

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -7,6 +7,7 @@
         </button>
     
         <div class="collapse navbar-collapse" id="mobile-nav">
+            <!-- Left Side Of Navbar -->
             <ul class="nav navbar-nav">
                 <li><a href="/" class="nav-link">Home</a></li>
                 <li><a href="/about" class="nav-link">About</a></li>
@@ -14,8 +15,30 @@
                 <li><a href="/posts" class="nav-link">Blog</a></li>
             </ul>
             <ul class="nav navbar-nav navbar-right">
-                <li><a href="/posts/create" class="nav-link">Create Post</a></li>
+                <!-- Authentication Links -->
+                @if (Auth::guest())
+                    <li><a href="{{ route('login') }}">Login</a></li>
+                    <li><a href="{{ route('register') }}">Register</a></li>
+                @else
+                 
+                    <ul>
+                        <li><a href="/dashboard" class="nav-link">Dashboard</a></li>
+                        <li>
+                            <a href="{{ route('logout') }}"
+                            onclick="event.preventDefault();
+                            document.getElementById('logout-form').submit();"
+                            class="nav-link">
+                            Logout
+                            </a>
+
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                {{ csrf_field() }}
+                            </form>
+                        </li>
+                   
+                @endif
             </ul>
+            
         </div>
     </div>
     </nav>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,22 +1,33 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet"
-        href="https://bootswatch.com/4/cosmo/bootstrap.min.css"> 
-        <title>{{config('app.name','LSAPP')}}</title>
-    </head>
-    <body>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Styles -->
+    <link rel="stylesheet"
+    href="https://bootswatch.com/4/cosmo/bootstrap.min.css">
+</head>
+<body>
+    <div id="app">
         @include('inc.navbar')
         <div class="container">
             @include('inc.messages')
             @yield('content')
         </div>
-       
-        <script src="//cdn.ckeditor.com/4.13.1/standard/ckeditor.js"></script>
-        <script>
-            CKEDITOR.replace( 'summary-ckeditor' );
-        </script>
-    </body>
-</html>
+    </div>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}"></script>
+    <script src="/vendor/unisharp/laravel-ckeditor/ckeditor.js"></script>
+    <script>
+        CKEDITOR.replace( 'article-ckeditor' );
+    </script>
+</body>
+</html
